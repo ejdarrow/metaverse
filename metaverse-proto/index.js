eb2048bf4b9ed42fc781034996628c352ce72160
Emily Jade Darrow <ejdarrow@icloud.com>
2020-03-22 12:36:16 -0400

    Rich chat

--- metaverse-proto/index.js
+++ metaverse-proto/index.js
@@ -1,20 +1,77 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
+var path = require('path');
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
 
+// The server plumbing
 
-
-app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html');
+http.listen(port, () => {
+	console.log("Server listening on %d", port);
 });
 
+
+//Routing
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Sockets
+//TODO: Implement max room size
+
+var numUsers = 0;
+
 io.on('connection', function(socket){
-	socket.on('chat message', function(msg){
-		io.emit('chat message', msg);
+	var addedUser = false;
+
+	socket.on('add user', (username) => {
+		socket.username = username;
+		++numUsers;		
+		addedUser = true;
+
+		//Here will go the max room size logic
+
+		socket.emit('login', {
+			numUsers: numUsers
+		});
+
+		socket.broadcast.emit('user joined', {
+			username: socket.username,
+			numUsers: numUsers
+		});
+	});
+	
+	socket.on('typing', () => {
+		socket.broadcast.emit('typing', {
+			username: socket.username
+		});
+	});
+
+	socket.on('stop typing', () => {
+		socket.broadcast.emit('stop typing', {
+			username: socket.username
+		});
+	});
+
+
+	socket.on('disconnect', () => {
+		if(addedUser){
+			--numUsers;
+		}
+		
+		socket.broadcast.emit('user left', {
+			username : socket.username,
+			numUsers : numUsers
+		});
+	});
+
+
+	socket.on('new message', function(msg){
+		socket.broadcast.emit('new message', {
+			username : socket.username,
+			message : msg
+		});
 	});
 });
 
-
-http.listen(3000, function(){
-	console.log('listening on *:3000');
-});
